68e57440035de59a111fe68d9b803c6f42f394e9
David <kioltk@gmail.com>
2013-10-23 02:17:30 +0300

    v0.1
    первый комит wp8 приложения

---
+++ wp8/wp8/MainPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using wp8.Resources;
+using wp8.Server;
+namespace wp8
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+
+        }
+
+        // Load data for the ViewModel Items
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            Connection.Text = "Подключение";
+            var asyncHandle = DataBase.Get(TestCallBack);
+            
+        }
+
+
+        public void TestCallBack(User result)
+        {
+            Connection.Text = result.name;
+            
+        }
+        // Sample code for building a localized ApplicationBar
+        //private void BuildLocalizedApplicationBar()
+        //{
+        //    // Set the page's ApplicationBar to a new instance of ApplicationBar.
+        //    ApplicationBar = new ApplicationBar();
+
+        //    // Create a new button and set the text value to the localized string from AppResources.
+        //    ApplicationBarIconButton appBarButton = new ApplicationBarIconButton(new Uri("/Assets/AppBar/appbar.add.rest.png", UriKind.Relative));
+        //    appBarButton.Text = AppResources.AppBarButtonText;
+        //    ApplicationBar.Buttons.Add(appBarButton);
+
+        //    // Create a new menu item with the localized string from AppResources.
+        //    ApplicationBarMenuItem appBarMenuItem = new ApplicationBarMenuItem(AppResources.AppBarMenuItemText);
+        //    ApplicationBar.MenuItems.Add(appBarMenuItem);
+        //}
+    }
+}
---
+++ wp8/wp8/Server/DataBase.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RestSharp;
+namespace wp8.Server
+{
+    public class User
+        {
+            public string name
+            {
+                get;
+                set;
+            }
+            public User()
+            {
+            }
+        }
+    public static class DataBase
+    {
+        
+        static RestClient Client = new RestClient("http://nocompany.azurewebsites.net/api/");
+
+        public static RestRequestAsyncHandle Get(Action<User> PageCallBack)
+        {
+            var request = new RestRequest("user/{method}", Method.GET);
+            request.AddUrlSegment("method" , "getbyid");
+            request.AddParameter("id", "1014");
+
+            
+            return Client.ExecuteAsync<User>(request, response =>
+            {
+                PageCallBack(response.Data);
+            });
+            
+        }
+
+        public static void test()
+        {
+            
+        }
+        
+    }
+}
